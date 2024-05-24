8af447ee9ccd5f3a91e81d650325a0ebda917555
NYAINA_FITIAVANA <nfitahiantsoa@ingenosya.mg>
2024-05-24 11:48:22 +0300

    integrated login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,19 +2,53 @@ import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {NzCardComponent} from "ng-zorro-antd/card";
 import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import {NzFormControlComponent, NzFormDirective} from "ng-zorro-antd/form";
+import {FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NzInputDirective, NzInputGroupComponent} from "ng-zorro-antd/input";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzIconDirective} from "ng-zorro-antd/icon";
 @Component({
-  selector: 'app-login',
+  selector: 'nz-demo-form-normal-login',
   standalone: true,
   imports: [
     NgOptimizedImage,
     NzCardComponent,
     NzRowDirective,
-    NzColDirective
+    NzColDirective,
+    NzFormDirective,
+    ReactiveFormsModule,
+    NzFormControlComponent,
+    NzInputGroupComponent,
+    NzInputDirective,
+    NzButtonComponent,
+    NzIconDirective
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 export class LoginComponent {
+  passwordVisible = false;
+  validateForm: FormGroup<{
+    email: FormControl<string>;
+    password: FormControl<string>;
+  }> = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  constructor(private fb: NonNullableFormBuilder) {}
 }
